5672fe9e493dfe46db8d254479c9de6b79442ebf
Shanmukh459 <saikrrish459@gmail.com>
2025-01-17 18:35:52 -0800

    work on put and delete methods

--- index.js
+++ index.js
@@ -26,15 +26,8 @@ app.get("/api/courses/:id", (req, res) => {
 })
 
 app.post("/api/courses", (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  }
-
-  const result = Joi.validate(req.body, schema)
-  if (!req.body.name || req.body.name.length < 3) {
-    res.status(400).send(result.error.details[0].message)
-    return
-  }
+  const { error } = validateCourse(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
   const course = {
     id: courses.length + 1,
@@ -43,6 +36,34 @@ app.post("/api/courses", (req, res) => {
   courses.push(course)
   res.send(course)
 })
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id))
+  if (!course) return res.status(404).send("The course doesn't exists.")
+
+  const { error } = validateCourse(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  course.name = req.body.name
+  res.send(course)
+})
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id))
+  if (!course) return res.status(404).send("This course doesn't exist.")
+
+  const index = courses.indexOf(course)
+  courses.splice(index, 1)
+  res.send(course)
+})
+
+function validateCourse(course) {
+  console.log(course)
+  const schema = {
+    name: Joi.string().min(3).required(),
+  }
+  return Joi.validate(course, schema)
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
