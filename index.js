7f4e6c87493fb3c86a880616bc87cbf96b9caae0
Shanmukh459 <saikrrish459@gmail.com>
2025-01-20 11:26:02 -0800

    work with configuration for different environments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("config")
 const morgan = require("morgan")
 const helmet = require("helmet")
 const Joi = require("joi")
@@ -10,7 +11,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use(helmet())
-app.use(morgan("tiny"))
+
+console.log(`Application name: ${config.get("name")}`)
+console.log(`Mail server: ${config.get("mail.host")}`)
+console.log(`Mail password: ${config.get("mail.password")}`)
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"))
+  console.log("Morgan enabled...")
+}
 
 app.use(log)
 app.use(auth)
