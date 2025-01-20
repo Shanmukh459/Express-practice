47ed6a29cb2b68172a745fc8d2e4e37233a788c8
Shanmukh459 <saikrrish459@gmail.com>
2025-01-20 12:10:12 -0800

    restructure the code

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const config = require("config")
 const morgan = require("morgan")
 const helmet = require("helmet")
 const Joi = require("joi")
-const log = require("./logger")
+const log = require("./middleware/logger")
+const courses = require("./routes/courses")
+const home = require("./routes/home")
 const auth = require("./auth")
 const express = require("express")
 const app = express()
@@ -16,6 +18,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use(helmet())
+
+app.use("/", home)
+app.use("/api.courses", courses)
 
 console.log(`Application name: ${config.get("name")}`)
 console.log(`Mail server: ${config.get("mail.host")}`)
@@ -31,67 +36,6 @@ dbDebugger("connected to database...")
 
 app.use(log)
 app.use(auth)
-
-const courses = [
-  { id: 1, name: "course1" },
-  { id: 2, name: "course2" },
-  { id: 3, name: "course3" },
-]
-
-app.get("/", (req, res) => {
-  res.render("index", { title: "My Express App", message: "Hello" })
-})
-
-app.get("/api/courses", (req, res) => {
-  res.send(courses)
-})
-
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id))
-  if (!course)
-    return res.status(404).send("The course with given id doesn't exist.")
-  return res.send(course)
-})
-
-app.post("/api/courses", (req, res) => {
-  const { error } = validateCourse(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  }
-  courses.push(course)
-  res.send(course)
-})
-
-app.put("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id))
-  if (!course) return res.status(404).send("The course doesn't exists.")
-
-  const { error } = validateCourse(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
-
-  course.name = req.body.name
-  res.send(course)
-})
-
-app.delete("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id))
-  if (!course) return res.status(404).send("This course doesn't exist.")
-
-  const index = courses.indexOf(course)
-  courses.splice(index, 1)
-  res.send(course)
-})
-
-function validateCourse(course) {
-  console.log(course)
-  const schema = {
-    name: Joi.string().min(3).required(),
-  }
-  return Joi.validate(course, schema)
-}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
